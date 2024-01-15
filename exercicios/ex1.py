ad5a4b614fd699aa211e16a6dfdc79835347d044
Rafael Alves da Silva <rafassilva0@gmail.com>
2024-01-15 12:35:10 -0300

    Substituindo a funçâo max()

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -4,13 +4,19 @@
 # Calcule e imprima a soma de todos os elementos do array:
 numeros = list(input("Digite os números que deseja:"))
 soma = 0
+max_valor = 0
+n = 0
+
 for num in numeros:
     soma += int(num)
+    if str(max_valor) <= numeros[n]:
+        max_valor = numeros[n]
+    n+=1
 
 print(f'A soma dos valores digitados é: {soma}')
-
+    
 # Encontre o valor máximo no array e o exiba:
-print(f'O maior valor dessa lista é: ',max(numeros))
+print(f'O maior valor dessa lista é: {max_valor}')
 
 # Inverta a ordem dos elementos no array e mostre o resultado:
 i = 0
